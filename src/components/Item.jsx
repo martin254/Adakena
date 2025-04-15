8912ae77cf0abb35e2e3b7ae8fa3c840671de073
martin254 <mutwiri333@gmail.com>
2025-04-15 10:56:57 +0100

    Proposed changes done-update item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,24 +7,25 @@ const Item = ({ book }) => {
   const { currency, addToCart } = useContext(ShopContext)
 
   return (
-    <div className="flex flex-col justify-between h-[420px] max-w-[250px] mx-auto transition-transform transform hover:scale-105 hover:animate-tilt duration-300">
-      {/* Image wrapped in Link */}
+    <div className="transition-transform transform hover:scale-105 duration-300">
+      {/* 🖼️ Image Container with nice white padding */}
       <Link to={`/book/${book._id}`}>
-        <div className="flexCenter bg-primary p-4 rounded-3xl overflow-hidden relative group h-[260px]">
+        <div className="bg-white rounded-2xl p-4 shadow-md">
           <img
             src={book.image}
             alt={book.name}
-            className="h-full w-auto max-h-[200px] object-contain mx-auto"
+            className="w-full h-[260px] object-contain rounded-lg mx-auto"
           />
         </div>
       </Link>
 
-      {/* Book Info */}
-      <div className="p-3 flex flex-col justify-between h-full">
+      {/* 📘 Book Info Section */}
+      <div className="p-3">
         <div className="flexBetween">
           <Link to={`/book/${book._id}`} className="hover:underline">
             <h4 className="h4 line-clamp-1 !my-0">{book.name}</h4>
           </Link>
+
           <span
             onClick={() => addToCart(book._id)}
             className="flexCenter h-8 w-8 rounded cursor-pointer hover:bg-primary"
@@ -34,8 +35,8 @@ const Item = ({ book }) => {
         </div>
 
         <div className="flexBetween pt-1">
-          <p className="font-bold capitalize">{book.category}</p>
-          <h5 className="h5 text-amber-900 pr-2">
+          <p className="font-bold capitalize text-gray-600">{book.category}</p>
+          <h5 className="h5 text-amber-900 pr-2 font-semibold">
             {currency}
             {book.price}.00
           </h5>
